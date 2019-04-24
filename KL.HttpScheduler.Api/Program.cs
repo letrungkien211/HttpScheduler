50124e5e9af4e2d029f72057d2af9aeedc4aab2f
letrungkien211 <letrungkien211@gmail.com>
2019-04-24 16:44:12 +0900

    add application insights

--- KL.HttpScheduler.Api/Program.cs
+++ KL.HttpScheduler.Api/Program.cs
@@ -19,6 +19,7 @@ namespace KL.HttpScheduler.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
