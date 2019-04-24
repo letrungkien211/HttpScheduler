0276d20cc09e2114068ea1e9f596b8a3c82fc2db
letrungkien211 <letrungkien211@gmail.com>
2019-04-24 19:08:26 +0900

    build ps1

---
+++ build.ps1
@@ -0,0 +1,9 @@
+﻿$acr =$acrName + "azurecr.io"
+$fulltag = $acr +"/httpschedulerrunner:" + $(Get-Date -format "yyyy-MM-dd-HH-mm")
+
+
+docker build -f KL.HttpScheduler.Runner/Dockerfile -t $fulltag .
+
+az acr login --name $acrName
+
+docker push $fulltag
