d29e83932d2eb6b17e23c412e9ced02d11da6c4a
Le Trung Kien <letrungkien211@gmail.com>
2019-05-18 14:59:00 +0000

    add more logs

--- KL.HttpScheduler/SchedulerRunner.cs
+++ KL.HttpScheduler/SchedulerRunner.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -15,14 +17,16 @@ namespace KL.HttpScheduler
     {
         private SortedSetScheduleClient SortedSetDequeueClient { get; }
         private MyActionBlock ActionBlock { get; }
+        private TelemetryClient TelemetryClient { get; }
         public SchedulerRunner(
             SortedSetScheduleClient sortedSetDequeueClient, 
-            MyActionBlock actionBlock
+            MyActionBlock actionBlock,
+            TelemetryClient telemetryClient
             )
         {
             SortedSetDequeueClient = sortedSetDequeueClient;
-
             ActionBlock = actionBlock;
+            TelemetryClient = telemetryClient;
         }
         /// <summary>
         /// Run async
@@ -36,13 +40,10 @@ namespace KL.HttpScheduler
                 var httpJob = await SortedSetDequeueClient.DequeueAsync(cancellationToken).ConfigureAwait(false);
                 if (httpJob != null)
                 {
-                    if(!ActionBlock.Post(httpJob)){
-                        // Put error log here.
-                    }
-                    else
-                    {
-
-                    }
+                    var success = ActionBlock.Post(httpJob);
+                    var telemetry = new EventTelemetry(success ? "EnqueueSuccess": "EnqueueFailure");
+                    telemetry.Context.Session.Id = httpJob.Id;
+                    TelemetryClient.TrackEvent(telemetry);
                 }
                 else
                 {
