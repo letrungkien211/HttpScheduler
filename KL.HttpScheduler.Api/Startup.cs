e3a50b68d37021e4cc2ec0f50094cfb6ec8cd2fa
Le Trung Kien <letrungkien211@gmail.com>
2019-05-21 18:56:04 +0900

    OK

--- KL.HttpScheduler.Api/Startup.cs
+++ KL.HttpScheduler.Api/Startup.cs
@@ -89,7 +89,6 @@ namespace KL.HttpScheduler.Api
                     );
             });
 
-            services.AddRouting(options => options.LowercaseUrls = true);
             services.AddSingleton<IJobProcessor, HttpJobProcessor>();
             services.AddSingleton<JobProcessorWrapper>();
             services.AddSingleton<TelemetryClient>();
