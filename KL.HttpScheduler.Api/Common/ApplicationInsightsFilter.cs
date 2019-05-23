d05a0adae6e826b24b31742a709b81255170a7e8
Le Trung Kien <letrungkien211@gmail.com>
2019-05-23 23:32:09 +0900

    success if status is 4xx

--- KL.HttpScheduler.Api/Common/ApplicationInsightsFilter.cs
+++ KL.HttpScheduler.Api/Common/ApplicationInsightsFilter.cs
@@ -27,11 +27,22 @@ namespace KL.HttpScheduler.Api.Common
                     {
                         return;
                     }
+
+                    // Success if status code is 4xx
+                    if (int.TryParse(request.ResponseCode, out var code))
+                    {
+                        if (code >= 400 && code < 500)
+                        {
+                            // If we set the Success property, the SDK won't change it:
+                            request.Success = true;
+                            // Allow us to filter these requests in the portal:
+                            request.Properties["Overridden400s"] = "true";
+                        }
+                    }
                     break;
                 default:
                     break;
             }
-
 
             Next.Process(item);
         }
